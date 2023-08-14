486758a0c75f6390cea5337d4999b14a5c2f4742
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2023-08-14 18:14:03 +0300

    js
    - correction;

--- js/js/01-show-hide-block.js
+++ js/js/01-show-hide-block.js
@@ -3,11 +3,21 @@ const content = document.querySelector('#content');
 console.log(button);
 console.log(content);
 button.addEventListener('click', function(){
-    content.classList.toggle('content-hidden');
+    
+    if(content.classList.toggle('content-hidden')) {
+        button.textContent = "Открыть блок";
+    } else {
+        button.textContent = "Закрыть блок";
+    }
+     
 
+
+
+    /*
     if(content.classList.contains('content-hidden')) {
         button.textContent = "Открыть блок";
     } else {
         button.textContent = "Закрыть блок";
     }
+    */
 });
