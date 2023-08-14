9fc821998adcf4f3b3f667c3cc829a08fff81f76
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2023-08-14 18:09:58 +0300

    js
    - open/close block with "if";

--- js/js/01-show-hide-block.js
+++ js/js/01-show-hide-block.js
@@ -4,4 +4,10 @@ console.log(button);
 console.log(content);
 button.addEventListener('click', function(){
     content.classList.toggle('content-hidden');
+
+    if(content.classList.contains('content-hidden')) {
+        button.textContent = "Открыть блок";
+    } else {
+        button.textContent = "Закрыть блок";
+    }
 });
