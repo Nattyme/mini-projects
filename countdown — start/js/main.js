71934ed95b507b1e4ff1fcaa1f116511538ffa2c
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2023-10-21 19:18:14 +0300

    Countdown - JS: data and time added to the page

--- countdown — start/js/main.js
+++ countdown — start/js/main.js
@@ -1,8 +1,17 @@
+//Определяем действующие элементы на странице
+const year = document.querySelector('#year'); //Ищем  место для года
+const days = document.querySelector('#days'); //Ищем место для дней
+const hours = document.querySelector('#hours'); // Ищем место для часов
+const minutes = document.querySelector('#minutes'); // Ищем место для минут
+const seconds = document.querySelector('#seconds'); //Ищем место для секунд
 
 
 // Делаем расчеты
 const currentYear = new Date().getFullYear();
 const nextYear = new Date(`January 01 ${currentYear + 1} 00:00:00`); // берем текущий год и время
+// Устанавливаем год на страницу
+year.innerText = currentYear + 1;
+
 
 // текущая дата и время
 const currentTime = new Date(); 
@@ -12,12 +21,19 @@ const difference = nextYear - currentTime;
 
 //Перевод в сек>минуты>часы>сутки, округление
 const daysLeft = Math.floor(difference / 1000 / 60 / 60 / 24); 
+
 //  Всего часов осталось. Далее берем остаток от деления на 24(преобразование в дни) и получаем часы неполного дня
 const hoursLeft = Math.floor(difference / 1000 / 60 / 60) % 24;
 
+
 //  Всего минут осталось. Далее берем остаток от деления на 60(преобразование в минуты) и получаем минуты неполного дня
 const minutesLeft = Math.floor(difference / 1000 / 60 ) % 60;
+
 //  Всего сек осталось. Далее берем остаток от деления на 60(преобразование в сек) и получаем сек неполного дня
 const secLeft = Math.floor(difference / 1000) % 60;
 
-console.log(daysLeft, hoursLeft, minutesLeft, secLeft )
+// Подставляем нужные значения на страницу
+days.innerText = daysLeft < 10 ? '0' + daysLeft : daysLeft;
+hours.innerText = hoursLeft < 10 ? '0' + hoursLeft : hoursLeft;
+minutes.innerText = minutesLeft < 10 ? '0' + minutesLeft : minutesLeft;
+seconds.innerText = secLeft < 10 ? '0' + secLeft : secLeft;
