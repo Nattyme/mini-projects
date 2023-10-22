5310ea586cbf78c5eaa85dcda99aa247570f79f7
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2023-10-22 23:22:29 +0300

    ToDo app: refactoring , function addTask

--- Java-script/ToDo app/js/main.js
+++ Java-script/ToDo app/js/main.js
@@ -5,40 +5,39 @@ const tasksList = document.querySelector('#tasksList');
 const emptyList = document.querySelector('#emptyList');
 
 
-form.addEventListener('submit', function(event){
-    // Отменяем отправку формы
-    event.preventDefault();
+form.addEventListener('submit', addTask);
 
-    // Достаем текст задачи из поля ввода
-    const taskText = taskInput.value;
+function addTask (event) {
+        // Отменяем отправку формы
+        event.preventDefault();
 
-    //Формируем разметку для новой задачи 
-    const taskHTML = `
-    <li class="list-group-item d-flex justify-content-between task-item">
-					<span class="task-title">${taskText}</span>
-					<div class="task-item__buttons">
-						<button type="button" data-action="done" class="btn-action">
-							<img src="./img/tick.svg" alt="Done" width="18" height="18">
-						</button>
-						<button type="button" data-action="delete" class="btn-action">
-							<img src="./img/cross.svg" alt="Done" width="18" height="18">
-						</button>
-					</div>
-				</li>
-    `;
-
-    //Добавляем на страницу
-tasksList.insertAdjacentHTML('beforeend', taskHTML);
+        // Достаем текст задачи из поля ввода
+        const taskText = taskInput.value;
     
-// Очищаем поле ввода и возвращаем на него фокус
-taskInput.value = "";
-taskInput.focus();
-
-// Проверка. Если в списке задач больше 1го элемента, то скрываем блок "Список задач"
-if(tasksList.children.length > 1) {
-    emptyList.classList.add('none');
+        //Формируем разметку для новой задачи 
+        const taskHTML = `
+        <li class="list-group-item d-flex justify-content-between task-item">
+                        <span class="task-title">${taskText}</span>
+                        <div class="task-item__buttons">
+                            <button type="button" data-action="done" class="btn-action">
+                                <img src="./img/tick.svg" alt="Done" width="18" height="18">
+                            </button>
+                            <button type="button" data-action="delete" class="btn-action">
+                                <img src="./img/cross.svg" alt="Done" width="18" height="18">
+                            </button>
+                        </div>
+                    </li>
+        `;
+    
+        //Добавляем на страницу
+    tasksList.insertAdjacentHTML('beforeend', taskHTML);
+        
+    // Очищаем поле ввода и возвращаем на него фокус
+    taskInput.value = "";
+    taskInput.focus();
+    
+    // Проверка. Если в списке задач больше 1го элемента, то скрываем блок "Список задач"
+    if(tasksList.children.length > 1) {
+        emptyList.classList.add('none');
+    }
 }
-console.log(tasksList);
-});
-
-
