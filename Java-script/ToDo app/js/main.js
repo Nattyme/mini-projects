49e43d9c59c788e965ade568af1ad6fe98bf49cb
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2023-10-22 23:05:57 +0300

    ToDo app: add task

--- Java-script/ToDo app/js/main.js
+++ Java-script/ToDo app/js/main.js
@@ -1,4 +1,31 @@
 // Находим элементы на странице
 const form = document.querySelector('#form');
-console.log(form);
+const taskInput =  document.querySelector('#taskInput');
+const tasksList = document.querySelector('#tasksList');
 
+form.addEventListener('submit', function(event){
+    // Отменяем отправку формы
+    event.preventDefault();
+
+    // Достаем текст задачи из поля ввода
+    const taskText = taskInput.value;
+
+    //Формируем разметку для новой задачи 
+    const taskHTML = `
+    <li class="list-group-item d-flex justify-content-between task-item">
+					<span class="task-title">${taskText}</span>
+					<div class="task-item__buttons">
+						<button type="button" data-action="done" class="btn-action">
+							<img src="./img/tick.svg" alt="Done" width="18" height="18">
+						</button>
+						<button type="button" data-action="delete" class="btn-action">
+							<img src="./img/cross.svg" alt="Done" width="18" height="18">
+						</button>
+					</div>
+				</li>
+    `;
+
+    //Добавляем на страницу
+tasksList.insertAdjacentHTML('beforeend', taskHTML)
+    console.log(tasksList);
+})
