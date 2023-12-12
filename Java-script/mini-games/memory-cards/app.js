94f9a3aeb08b49eba4f767e595ee415fd98c81b8
NeuralNat <ifrance@yandex.ru>
2023-12-12 20:49:04 +0300

    memory-game base files

---
+++ Java-script/mini-games/memory-cards/app.js
@@ -0,0 +1,53 @@
+const cardArray = [
+    {
+        name: 'fries',
+        img: './images/fries.jpg',
+    },
+    {
+        name: 'cheeseburger',
+        img: './images/cheeseburger.jpg',
+    },
+    {
+        name: 'milkshake',
+        img: './images/milkshake.jpg',
+    },
+    {
+        name: 'hotdog',
+        img: './images/hotdog.jpg',
+    },
+    {
+        name: 'ice-cream',
+        img: './images/ice-cream.jpg',
+    },
+    {
+        name: 'pizza',
+        img: './images/pizza.jpg',
+    },
+    {
+        name: 'fries',
+        img: './images/fries.jpg',
+    },
+    {
+        name: 'cheeseburger',
+        img: './images/cheeseburger.jpg',
+    },
+    {
+        name: 'milkshake',
+        img: './images/milkshake.jpg',
+    },
+    {
+        name: 'hotdog',
+        img: './images/hotdog.jpg',
+    },
+    {
+        name: 'ice-cream',
+        img: './images/ice-cream.jpg',
+    },
+    {
+        name: 'pizza',
+        img: './images/pizza.jpg',
+    }
+]
+
+console.log(cardArray);
+
