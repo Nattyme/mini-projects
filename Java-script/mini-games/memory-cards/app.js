f794ec87ec727fb83fa9e946f80cce2f8a07c69d
NeuralNat <ifrance@yandex.ru>
2023-12-13 19:59:31 +0300

    JS if double click for the same card

--- Java-script/mini-games/memory-cards/app.js
+++ Java-script/mini-games/memory-cards/app.js
@@ -52,8 +52,9 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random());
 
 const gridDisplay = document.querySelector('#grid');
-const cardsChosen = []
-const cardsChosenIds = []
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
 
 function createBoard () {
     for (let i = 0; i < cardArray.length; i++) {
@@ -68,12 +69,22 @@ createBoard()
 
 function checkMatch() {
     const cards = document.querySelectorAll('img');
-
+    const optionOneId = cardsChosenIds[0]
+    const optionTwoId = cardsChosenIds[1]
     console.log('checkMAtch!!!');
-    if (cardsChosen[0] == cardsChosen[1]) {
-        alert('you found a match!')
-        cards[cardsChosenIds[0]].setAttribute('src', './images/white.png')
+    if( optionOneId == optionTwoId) {
+        alert('You have clicked the same image!')
     }
+    if (cardsChosen[0] == cardsChosen[1]) {
+        alert('You found a match!')
+        cards[optionOneId].setAttribute('src', './images/white.png');
+        cards[optionTwoId].setAttribute('src', './images/white.png');
+        cards[optionOneId].removeEventListener('click', flipCard);
+        cards[optionTwoId].removeEventListener('click', flipCard);
+        cardsWon.push(cardsChosen)
+    }
+    cardsChosen = []
+    cardsChosenIds = []
 }
 
 function flipCard () {
