7427331d50a51bc0fb572514a6f4ffe973af7943
NeuralNat <ifrance@yandex.ru>
2023-12-13 19:47:50 +0300

    JS show white img if match

--- Java-script/mini-games/memory-cards/app.js
+++ Java-script/mini-games/memory-cards/app.js
@@ -1,51 +1,51 @@
 const cardArray = [
     {
         name: 'fries',
-        img: './images/fries.jpg',
+        img: './images/fries.png',
     },
     {
         name: 'cheeseburger',
-        img: './images/cheeseburger.jpg',
+        img: './images/cheeseburger.png',
     },
     {
         name: 'milkshake',
-        img: './images/milkshake.jpg',
+        img: './images/milkshake.png',
     },
     {
         name: 'hotdog',
-        img: './images/hotdog.jpg',
+        img: './images/hotdog.png',
     },
     {
         name: 'ice-cream',
-        img: './images/ice-cream.jpg',
+        img: './images/ice-cream.png',
     },
     {
         name: 'pizza',
-        img: './images/pizza.jpg',
+        img: './images/pizza.png',
     },
     {
         name: 'fries',
-        img: './images/fries.jpg',
+        img: './images/fries.png',
     },
     {
         name: 'cheeseburger',
-        img: './images/cheeseburger.jpg',
+        img: './images/cheeseburger.png',
     },
     {
         name: 'milkshake',
-        img: './images/milkshake.jpg',
+        img: './images/milkshake.png',
     },
     {
         name: 'hotdog',
-        img: './images/hotdog.jpg',
+        img: './images/hotdog.png',
     },
     {
         name: 'ice-cream',
-        img: './images/ice-cream.jpg',
+        img: './images/ice-cream.png',
     },
     {
         name: 'pizza',
-        img: './images/pizza.jpg',
+        img: './images/pizza.png',
     }
 ]
 
@@ -53,6 +53,7 @@ cardArray.sort(() => 0.5 - Math.random());
 
 const gridDisplay = document.querySelector('#grid');
 const cardsChosen = []
+const cardsChosenIds = []
 
 function createBoard () {
     for (let i = 0; i < cardArray.length; i++) {
@@ -65,13 +66,29 @@ function createBoard () {
 }
 createBoard()
 
+function checkMatch() {
+    const cards = document.querySelectorAll('img');
+
+    console.log('checkMAtch!!!');
+    if (cardsChosen[0] == cardsChosen[1]) {
+        alert('you found a match!')
+        cards[cardsChosenIds[0]].setAttribute('src', './images/white.png')
+    }
+}
+
 function flipCard () {
     console.log(cardArray);
     const cardId = this.getAttribute('data-id');
     cardsChosen.push(cardArray[cardId].name);
+    cardsChosenIds.push(cardId)
+    console.log(cardsChosen)
+    console.log(cardsChosenIds)
     console.log('clicked', cardId);
     console.log(cardsChosen);
     this.setAttribute('src', cardArray[cardId].img)
+    if(cardsChosen.length === 2) {
+        setTimeout(checkMatch,500);
+    }
 }
 
 
