4131e99e6eb5f80207e8c07dfb8bd6d7d98f2930
NeuralNat <ifrance@yandex.ru>
2023-12-13 20:12:44 +0300

    JS score for every match , result in the end

--- Java-script/mini-games/memory-cards/app.js
+++ Java-script/mini-games/memory-cards/app.js
@@ -52,6 +52,7 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random());
 
 const gridDisplay = document.querySelector('#grid');
+const resultDisplay = document.querySelector('#result')
 let cardsChosen = []
 let cardsChosenIds = []
 const cardsWon = []
@@ -72,7 +73,9 @@ function checkMatch() {
     const optionOneId = cardsChosenIds[0]
     const optionTwoId = cardsChosenIds[1]
     console.log('checkMAtch!!!');
-    if( optionOneId == optionTwoId) {
+    if(optionOneId == optionTwoId) {
+        cards[optionOneId].setAttribute('src','./images/blank.png')
+        cards[optionTwoId].setAttribute('src','./images/blank.png')
         alert('You have clicked the same image!')
     }
     if (cardsChosen[0] == cardsChosen[1]) {
@@ -82,9 +85,18 @@ function checkMatch() {
         cards[optionOneId].removeEventListener('click', flipCard);
         cards[optionTwoId].removeEventListener('click', flipCard);
         cardsWon.push(cardsChosen)
+    } else {
+        cards[optionOneId].setAttribute('src', './images/blank.png')
+        cards[optionTwoId].setAttribute('src', './images/blank.png')
+        alert('Sorry, try again!')
     }
+    resultDisplay.textContent = cardsWon.length;
     cardsChosen = []
     cardsChosenIds = []
+
+    if (cardsWon.length == cardArray.length/2) {
+        resultDisplay.innerHTML = 'Congratulations you found them all'
+    }
 }
 
 function flipCard () {
