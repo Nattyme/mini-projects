25773e0b35a1515a022fef53ffe1e8009b8e3328
NeuralNat <ifrance@yandex.ru>
2023-12-14 11:48:35 +0300

    JS function for random squares

--- Java-script/mini-games/whac-a-mole/app.js
+++ Java-script/mini-games/whac-a-mole/app.js
@@ -1,2 +1,21 @@
 const squares = document.querySelectorAll('.square');
 const mole = document.querySelector('.mole');
+const timeLeft = document.querySelector('#time-left');
+const scrore = document.querySelector('#score');
+
+let result = 0;
+
+function randomSquare () {
+    squares.forEach(square => {
+        square.classList.remove('mole')
+    });
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)];
+    randomSquare.classList.add('mole');
+}
+
+function  moveMole() {
+    let timeId = null;
+    timeId = setInterval(randomSquare, 500);
+}
+moveMole();
