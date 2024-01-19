0d64eb7ce25173d7b91b7616edc6033b22417f91
NeuralNat <ifrance@yandex.ru>
2024-01-19 23:53:38 +0300

    JS form send if input visible

--- page components/search-form/js/main.js
+++ page components/search-form/js/main.js
@@ -2,6 +2,8 @@ const searchForm = document.querySelector('.search');
 const searchButton = document.querySelector('.search__button');
 
 searchButton.addEventListener('click', function(e){
-    e.preventDefault();
-    searchForm.classList.toggle('search--visible');
+    if(!searchForm.classList.contains('search--visible')){
+        e.preventDefault();
+        searchForm.classList.toggle('search--visible');
+    }
 });
