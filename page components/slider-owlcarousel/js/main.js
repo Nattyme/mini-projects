fd41017671c1fc4134c64e2a4eb3927b600cdf55
NeuralNat <ifrance@yandex.ru>
2024-01-20 21:19:07 +0300

    SCSS custom slider button svg style

--- page components/slider-owlcarousel/js/main.js
+++ page components/slider-owlcarousel/js/main.js
@@ -1,9 +1,32 @@
 $(document).ready(function() {
-    $('#photo-carousel').owlCarousel({
+    // $('#photo-carousel').owlCarousel({
+    //     items: 1, 
+    //     loop: true,
+    //     nav: true,
+    //     navText: ['Prev', 'Next']
+    // });
+
+    // Находим элемент в котором будет инициализирована карусель
+    const photoCarousel = $('#photo-carousel');
+
+    // Создаем карусель в элементе #photo-carousel
+    photoCarousel.owlCarousel({
         items: 1, 
         loop: true,
         nav: true,
         navText: ['Prev', 'Next']
+    });
+
+    // Находим кнопки назад и вперед
+    const btnPrev = $('.nav-btn-left');
+    const btnNext = $('.nav-btn-right');
+
+    btnPrev.click(function(){
+        photoCarousel.trigger('prev.owl.carousel')
+    });
+
+    btnNext.click(function(){
+        photoCarousel.trigger('next.owl.carousel')
     });
 });
 
