c1cc55dcc61a88de51860b5307d9b23a145a7a6e
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-07-03 23:47:45 +0300

    php-filmoteka index.php correction

--- php/filmoteka/index.php
+++ php/filmoteka/index.php
@@ -1,126 +1,157 @@
-<?php
-    require("./config.php");
-    $headName = "Наталья Балашова";
+<?php 
 
-    //Подключение к БД
-    $link = mysqli_connect('localhost', 'root', '', 'filmoteka');
+// DB CONNECTION
+$link = mysqli_connect('localhost', 'root', '', 'filmoteka');
 
-    //Проверка подключения
-    if (mysqli_connect_errno()) {
-        printf("Ошибка подключения к базе данных.: %s\n", mysqli_connect_error());
-        exit();
-    }
+if ( mysqli_connect_error() ) {
+	die("Ошибка подключения к базе данных.");
+}
 
-    //Если форма отправлена, проверяем данные на пробелы/код/спецсимволы и записываем в БД
-    if(array_key_exists('newFilm', $_POST)) {
-        if(empty(trim($_POST['title'])) ) {
-            // echo "Название фильма не может быть пустым.";
-        } else {
-            $query = "INSERT INTO `films` (`title`, `genre`, `year`) VALUES (
-                ' ".mysqli_real_escape_string($link, trim(strip_tags($_POST['title'])) )." ',
-                ' ".mysqli_real_escape_string($link, strip_tags(trim($_POST['genre'])) )." ',
-                ' ".mysqli_real_escape_string($link, strip_tags(trim($_POST['year']))  )." '
-                )";
-         
-            if ( mysqli_query($link, $query) ) {
-                $success = true;
-        
-                // Сброс POST массива
-                foreach ($_POST as $key => $value) {
-                    unset($_POST[$key]);
-                }
-            } else {
-                $failure = true;
-            }
-        }
-    }
 
-    //Выводим карточки фильмов из БД и записываем в новый массив filmsList
-    $query = "SELECT * FROM `films`";
-    $filmsList = array();
+// Save form data to DB
 
-    if( $result = mysqli_query($link, $query) ) {
-        while($row = mysqli_fetch_array($result) ) {
-            $filmsList[] = $row;
-        }
-    };
+$resultSuccess = "";
+$resultError = "";
+$errors = array();
+
+if ( array_key_exists('add-film', $_POST) ) {
+	
+	// Обработка ошибок
+	if ( $_POST['title'] == '') {
+		$errors[] = "<p>Необходимо ввести название фильма!</p>";
+	}
+	if ( $_POST['genre'] == '') {
+		$errors[] = "<p>Необходимо ввести жанр фильма!</p>";
+	}
+	if ( $_POST['year'] == '') {
+		$errors[] = "<p>Необходимо ввести год фильма!</p>";
+	}
+
+	// Если ошщибок нет - сохраняем фильм
+	if ( empty($errors) ) {
+		// Запись данных в БД
+		$query = "INSERT INTO films (title, genre, year) VALUES (
+			'". mysqli_real_escape_string($link, $_POST['title']) ."', 
+			'". mysqli_real_escape_string($link, $_POST['genre']) ."', 
+			'". mysqli_real_escape_string($link, $_POST['year']) ."'
+			)";
+
+		if ( mysqli_query($link, $query) ) {
+			$resultSuccess = "<p>Фильм был успешно добавлен!</p>";
+		} else { 
+			$resultError = "<p>Что то пошло не так. Добавьте фильм еще раз!</p>";
+		}
+	}
+
+
+
+}
+
+// echo "<pre>";
+// print_r($_POST);
+// echo "</pre>";
+
+
+// Getting films from DB
+$query = "SELECT * FROM films";
+$films = array();
+
+$result = mysqli_query($link, $query);
+
+if ( $result = mysqli_query($link, $query) ) {
+	while ( $row = mysqli_fetch_array($result)  ) {
+		$films[] = $row;
+	}
+}
+
+// echo "<pre>";
+// print_r($films);
+// echo "</pre>";
+
 ?>
 
-<!-- Разные миксины по одному, которые понадобятся. Для логотипа, бейджа, и т.д.-->
 <!DOCTYPE html>
 <html lang="ru">
-<?php include(ROOT . "templates/head.tpl") ?>
-<body class="index-page">
-	<div class="container user-content section-page">
-		<div class="title-1">Фильмотека</div>
-        <!-- cards rows -->
-		<?php 
-            foreach ($filmsList as $key => $value) {
-                $filmTitle = $filmsList[$key]['title'];
-                $filmGenre = $filmsList[$key]['genre'];
-                $filmYear =  $filmsList[$key]['year'];
+  <head>
+	<meta charset="UTF-8"/>
+	<title>Фильмотека</title>
+	<!--[if IE]>
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	<![endif]-->
+	<meta name="viewport" content="width=device-width,initial-scale=1"/>
+	<meta name="keywords" content=""/>
+	<meta name="description" content=""/><!-- build:cssVendor css/vendor.css -->
+	<link rel="stylesheet" href="libs/normalize-css/normalize.css"/>
+	<link rel="stylesheet" href="libs/bootstrap-4-grid/grid.min.css"/>
+	<link rel="stylesheet" href="libs/jquery-custom-scrollbar/jquery.custom-scrollbar.css"/><!-- endbuild -->
+<!-- build:cssCustom css/main.css -->
+	<link rel="stylesheet" href="./css/main.css"/><!-- endbuild -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800&amp;subset=cyrillic-ext" rel="stylesheet">
+<!--[if lt IE 9]>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script><![endif]-->
+  </head>
+  <body>
+	<div class="container user-content pt-35">
 
-                include(ROOT . "templates/widgets/cardFilm.tpl");
-            }
-        ?> 
-        <!--// cards rows -->
+	
 
-		<div class="panel-holder mt-80 mb-40">
-			<div class="title-3 mt-0">Добавить фильм</div>
-			<form action="index.php" method="POST">
-                
-                <?php if(isset($success) && $success) : ?>
-                    <div class="notify notify--success mb-20">Фильм был успешно добавлен!</div>
-                <?php endif; ?>
+	<?php if ( $resultSuccess != '' ) { ?> 
+		<div class="info-success"><?=$resultSuccess?></div>
+	<?php } ?>
 
-                <?php if(isset($_POST['newFilm']) && empty(trim($_POST['title'])) ) : ?>
-                    <div class="notify notify--error mb-20">Название фильма не может быть пустым.</div>
-                <?php endif; ?>
+	<?php if ( $resultError != '' ) { ?> 
+		<div class="error"><?=$resultError?></div>
+	<?php } ?>
 
-				<div class="form-group">
-                    <label class="label">
-                        Название фильма
-                        <input class="input" name="title" type="text" placeholder="Такси 2" />
-                    </label>
-                </div>
 
-				<div class="row">
-					<div class="col">
-						<div class="form-group">
-                            <label class="label">
-                                Жанр
-                                <?php if(isset($_POST['newFilm']) && !empty(trim($_POST['genre']))) : ?>
-                                    <input name="genre" type="text" class="input" value="<?php echo trim($_POST['genre']) ?>" />
-                                <?php else: ?>
-                                    <input class="input" name="genre" type="text" placeholder="комедия" />
-                                <?php endif; ?>
-                            </label>
-                        </div>
-					</div>
-					<div class="col">
-						<div class="form-group">
-                            <label class="label">
-                                Год
-                                <?php if(isset($_POST['newFilm']) && !empty(trim($_POST['year']))) : ?>
-                                    <input name="year" type="text" class="input" value="<?php echo trim($_POST['year']) ?>" />
-                                <?php else: ?>
-                                    <input class="input" name="year" type="text" placeholder="2000" />
-                                <?php endif; ?>
-                            </label>
-                        </div>
-					</div>
-				</div>
-                <input class="button" type="submit" name="newFilm" value="Добавить" />
-			</form>
+
+	<h1 class="title-1"> Фильмотека</h1>
+
+	<?php
+		foreach ($films as $key => $film) { ?>
+		<div class="card mb-20">
+			<h4 class="title-4"><?=$film['title']?></h4>
+            <a href="#!" class="button button--remove">Удалить</a>
+			<div class="badge"><?=$film['genre']?></div>
+			<div class="badge"><?=$film['year']?></div>
 		</div>
+	<?php } ?>
+
+	  <div class="panel-holder mt-80 mb-100">
+		<div class="title-4 mt-0">Добавить фильм</div>
+
+		<form action="index.php" method="POST">
+
+			<?php 
+			if ( !empty($errors)) {
+				foreach ($errors as $key => $value) {
+				echo "<div class='error'>$value</div>";
+				}
+			}
+			?>
+
+			<label class="label-title">Название фильма</label>
+			<input class="input" type="text" placeholder="Такси 2" name="title"/>
+			<div class="row">
+				<div class="col">
+					<label class="label-title">Жанр</label>
+					<input class="input" type="text" placeholder="комедия" name="genre"/>
+				</div>
+				<div class="col">
+					<label class="label-title">Год</label>
+					<input class="input" type="text" placeholder="2000" name="year"/>
+				</div>
+			</div>
+			<input type="submit" class="button" value="Добавить" name="add-film">
+		</form>
+
+	  </div>
 	</div><!-- build:jsLibs js/libs.js -->
 	<script src="libs/jquery/jquery.min.js"></script><!-- endbuild -->
-	<!-- build:jsVendor js/vendor.js -->
-	<script src="libs/jquery-custom-scrollbar/jquery.custom-scrollbar.js"></script>
-	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIr67yxxPmnF-xb4JVokCVGgLbPtuqxiA"></script><!-- endbuild -->
-	<!-- build:jsMain js/main.js -->
+<!-- build:jsVendor js/vendor.js -->
+	<script src="libs/jquery-custom-scrollbar/jquery.custom-scrollbar.js"></script><!-- endbuild -->
+<!-- build:jsMain js/main.js -->
 	<script src="js/main.js"></script><!-- endbuild -->
 	<script defer="defer" src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-</body>
-
+  </body>
 </html>
