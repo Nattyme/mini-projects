4668c8ed841a0cb8c45c3217fa89220a77935ed0
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-11-06 23:34:03 +0300

    correction checking for Infinite

--- js/calculator/main.js
+++ js/calculator/main.js
@@ -3,25 +3,29 @@ const calculateModule = ( function () {
   
   // Переменная для записи результата
   let result = 0;
-  let a = 0;
-  let b = 0;
+  let a;
+  let b;
 
    // Фун-ция устанав. значение
   function setValue (value_1, value_2) {
-   
+    if (!isFinite(value_1) || !isFinite(value_2)) {
+      return alert ('Невозможно обработать. Число слишком большое. ');
+    }
+    
     // Если введено не целое число - ошибка
-    if ( ! Number.isInteger(value_1) || ! Number.isInteger(value_2) ) {
+    if ( !Number.isInteger(value_1) || !Number.isInteger(value_2) ) {
       return console.log ('Введите число!');
     }
 
     // Если число меньше или равно 0 - ошибка
-    if ( ( value_1 || value_2 ) <= 0 && Number.isInteger(value_1) && Number.isInteger(value_2) ) {
-      alert (`Число должно быть больше \'0\'!`);
+    if ( ( value_1 <= 0 || value_2 <= 0 ) && Number.isInteger(value_1) && Number.isInteger(value_2) ) {
+      return alert (`Число должно быть больше \'0\'!`);
     }
+     
     
     a = value_1;
     b = value_2;
-   
+
   }
  
 
@@ -43,13 +47,16 @@ const calculateModule = ( function () {
   // Фун-ция выполняет деление
   function divide () {
     if ( b === 0) {
-      return console.log('Нельзя делить на 0');
+      return alert('Нельзя делить на 0');
     }
+    
     result = a / b;
   }
   
   //Фун-ция возвращает результат
   function  getresult () {
+    if(!result) return;
+    
     return console.log(result);
   }
   
@@ -63,7 +70,7 @@ const calculateModule = ( function () {
   }
   
 }) ();
-calculateModule.setValue(5, 0);
+calculateModule.setValue(Infinity, Infinity);
 // calculateModule.sum();
 // calculateModule.difference();
 calculateModule.divide();
