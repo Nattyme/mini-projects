02033982b447517639f7a046fdd25ad58ff33227
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-11-15 23:28:02 +0300

    budget-calculator: base dom element find, get data from form

--- js/budget-calculator/js/main.js
+++ js/budget-calculator/js/main.js
@@ -1,1 +1,13 @@
-console.log('Hello, world!)')
+const form = document.querySelector('#form');  // Найдем форму
+const type = form.querySelector('#type');     // Найдём селект в форме
+const title = form.querySelector('#title');     // Найдём инпут в форме
+const value = form.querySelector('#value');
+
+// Добавим прослушивание события submit
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  
+  console.log(type.value);
+  console.log(title.value);
+  console.log(value.value);
+});
