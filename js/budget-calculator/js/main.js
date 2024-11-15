3e62136c3193f3bd1c3bb0dbe4af29856880f73a
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-11-15 23:59:30 +0300

    budget-calculator: id count logic for records

--- js/budget-calculator/js/main.js
+++ js/budget-calculator/js/main.js
@@ -1,3 +1,7 @@
+// Data
+const budget = [];
+
+
 const form = document.querySelector('#form');  // Найдем форму
 const type = form.querySelector('#type');     // Найдём селект в форме
 const title = form.querySelector('#title');     // Найдём инпут в форме
@@ -10,4 +14,23 @@ form.addEventListener('submit', function (e) {
   console.log(type.value);
   console.log(title.value);
   console.log(value.value);
+
+  // Расчёт id
+  let id = 1;
+  if (budget.length > 0) {
+    const lastElement = budget[budget.length - 1];
+    const lastElemId = lastElement.id;
+    id = lastElemId + 1;
+  }
+
+  // Формирует запись
+  const record = {
+    id: id,
+    type : type.value,
+    title : title.value,
+    value : value.value
+  }
+  budget.push(record);
+  console.log(budget);
+  
 });
