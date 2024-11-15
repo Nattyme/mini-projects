2396b8f788efa871e302d680b19bc10305211e48
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-11-15 12:26:22 +0300

    to-do-mvc: model base

---
+++ js/to-do-mvc/js/todo/controller.js
@@ -0,0 +1,7 @@
+import Model from './model.js';
+const model = new Model ();
+
+console.log(model);
+model.addTask('Заверстать стартоый шаблон');
+model.addTask('Написать скрипт');
+console.log(model)
