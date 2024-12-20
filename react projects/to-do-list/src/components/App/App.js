1c8ed092727caf7a3e7d63dbd3b1d271528e1be5
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-12-21 00:40:39 +0300

    react-projects: to-do-list: footer component

--- react projects/to-do-list/src/components/App/App.js
+++ react projects/to-do-list/src/components/App/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import Header from './../Header/Header';
+import Footer from './../Footer/Footer';
 
 const App = () => {
   return (
     <body className="todo-app p-5">
       {<Header/>}
+      {<Footer/>}
     </body>
   )
 }
---
+++ react projects/to-do-list/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css';
+
+const Footer = () => {
+	return (
+		<footer class="footer">
+			<input type="text" placeholder="Что необходимо сделать" class="form-control me-2" />
+			<button type="button" class="btn btn-primary">Добавить</button>
+		</footer>
+	)
+}
+
+export default Footer;
