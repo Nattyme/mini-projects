a837c3343f3cd1f79baaec5a67dc270d77caa676
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-12-21 03:13:59 +0300

    react-projects: to-do-list: header tasks counter

--- react projects/to-do-list/src/components/Header/Header.js
+++ react projects/to-do-list/src/components/Header/Header.js
@@ -1,10 +1,15 @@
 import './Header.css';
+import data from './../../data/data.json';
+
+const tasksProgress = (type) => {
+  return data.filter( (el) => el.status === type).length;
+}
 
 const Header = () => {
 	return (
 		<header className="header">
 			<h1 className="header-title">Список дел</h1>
-			<span className="header-desc">3 осталось, 0 сделано</span>
+			<span className="header-desc">{ tasksProgress('active') } осталось, {tasksProgress('done')} сделано</span>
 		</header>
 	)
 }
--- react projects/to-do-list/src/components/List/List.js
+++ react projects/to-do-list/src/components/List/List.js
@@ -1,36 +1,44 @@
 import './List.css';
 
+import data from './../../data/data.json';
 import Button from  './../Buttons/Buttons';
 
-const List = () => {
-return (
-	<ul className="todo-list">
+const getTitle = (tasksExist) => {
+	const text = tasksExist ? 'Список дел' : 'Список дел пуст';
+
+	return (
 		<li className="todo-item justify-content-center">
-			<span className="todo-item-text">Список дел пуст</span>
+			<span className="todo-item-text">{text}</span>
 		</li>
-		<li className="todo-item">
-			<span className="todo-item-text">Выпить кофе</span>
-			<div className="btn-group">
-				<button role="button" className="btn btn-outline-dark btn-sm">Важное</button>
-				<button role="button" className="btn btn-outline-danger btn-sm">Удалить</button>
-			</div>
-		</li>
-		<li className="todo-item">
-			<span className="todo-item-text">Сделать React приложение</span>
-			<div className="btn-group">
-				<button role="button" className="btn btn-outline-dark btn-sm">Важное</button>
-				<button role="button" className="btn btn-outline-danger btn-sm">Удалить</button>
-			</div>
-		</li>
-		<li className="todo-item">
-			<span className="todo-item-text">Позавтракать</span>
-			<div className="btn-group">
-        <Button classNames = 'btn-outline-dark btn-sm'  text= 'Важное' />
-        <Button classNames = 'btn-outline-danger btn-sm'  text= 'Удалить' />
-			</div>
-		</li>
-	</ul>
-)
+	);
+}
+
+const tasksExist = (data) => {
+	return data.length > 0 ? true : false;
+}
+
+const renderTasks = (data) => {
+	return data.map( (el) => {
+		return (
+			<li key={el.id} className="todo-item">
+				<span className="todo-item-text">{el.title}</span>
+				<div className="btn-group">
+					<Button classNames = 'btn-outline-dark btn-sm'  text= 'Важное' />
+					<Button classNames = 'btn-outline-danger btn-sm'  text= 'Удалить' />
+				</div>
+			</li>
+		)
+	});
+}
+
+
+const List = () => {
+	return (
+		<ul className="todo-list">
+			{ getTitle( tasksExist(data) )}
+			{ renderTasks( data ) }
+		</ul>
+	)
 }
 
 export default List;
