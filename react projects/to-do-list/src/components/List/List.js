22271fddcfdee409c5f5bf45e6a119e6e36bb4c3
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-12-21 17:12:55 +0300

    react-projects: to-do-list: listItem structure correction

--- react projects/to-do-list/src/components/List/List.js
+++ react projects/to-do-list/src/components/List/List.js
@@ -1,22 +1,15 @@
 import './List.css';
-import data from './../../data/data.json';
 import ListItem from './../ListItem/ListItem';
-
-const getTitle = (tasksExist) => {
-	return (
-		<li className="todo-item justify-content-center">
-			<span className="todo-item-text">{tasksExist ? 'Список дел' : 'Список дел пуст'}</span>
-		</li>
-	);
-}
+import data from './../../data/data.json';
 
 const List = () => {
-  const tasksExist = data.length > 0;
-
 	return (
 		<ul className="todo-list">
-      { getTitle(tasksExist) }
-      {<ListItem tasks= {data} />}
+        {
+          data.length > 0 ? 
+          data.map( (task) => <ListItem key={task.id} task = {task} />)
+          : <ListItem isEmpty = {true} />
+        }
 		</ul>
 	)
 }
--- react projects/to-do-list/src/components/ListItem/ListItem.js
+++ react projects/to-do-list/src/components/ListItem/ListItem.js
@@ -1,17 +1,32 @@
+import React from 'react'; // need to convert into class
 import Button from  './../Buttons/Buttons';
 
-const ListItem = ( {tasks} ) => {
-  return tasks.map( (el) => {
-		return (
-			<li key={el.id} className="todo-item">
-				<span className="todo-item-text">{el.title}</span>
-				<div className="btn-group">
-					<Button classNames = 'btn-outline-dark btn-sm'  text= 'Важное' />
-					<Button classNames = 'btn-outline-danger btn-sm'  text= 'Удалить' />
-				</div>
-			</li>
-		)
-	});
+class ListItem extends React.Component {
+	render () {
+		const {task, isEmpty} = this.props;
+
+		if (isEmpty) {
+			return (
+				<li className="todo-item justify-content-center">
+ 					<span className="todo-item-text">Список дел пуст</span>
+ 				</li>
+			)
+		}
+
+		if (task) {
+			return (
+				<li className="todo-item">
+					<span className="todo-item-text">{task.title}</span>
+					<div className="btn-group">
+						<Button classNames = 'btn-outline-dark btn-sm'  text= 'Важное' />
+						<Button classNames = 'btn-outline-danger btn-sm'  text= 'Удалить' />
+					</div>
+				</li>
+			);
+		}
+
+		return null;
+	};
 }
 
 export default ListItem;
