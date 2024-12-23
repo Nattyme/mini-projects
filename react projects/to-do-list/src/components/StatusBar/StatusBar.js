411853f347f6308d466c4ec40ec35df47b13edb7
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-12-23 23:33:44 +0300

    react-projects: to-do-list: button class changy by function

--- react projects/to-do-list/src/components/StatusBar/StatusBar.js
+++ react projects/to-do-list/src/components/StatusBar/StatusBar.js
@@ -2,11 +2,15 @@ import Button from  './../Button/Button';
 import './StatusBar.css';
 
 const StatusBar = (props) => {
+  const getClass = (buttonStatus) => {
+    return props.status === buttonStatus ? 'btn-primary' : 'btn-light';
+  }
+
 	return (
 		<div onClick={(e)=>{props.changeStatus(e.target.dataset.button)}} className="btn-group" role="group">
-			<Button classNames={props.status === 'all' ? 'btn-primary' : 'btn-light'}  dataset={'all'} text= 'Все' />
-			<Button classNames={props.status === 'active' ? 'btn-primary' : 'btn-light'} dataset={'active'} text= 'Активные' />
-			<Button classNames={props.status === 'done' ? 'btn-primary' : 'btn-light'}  dataset={'done'} text= 'Выполненные' />
+			<Button classNames={getClass('all')}  dataset={'all'} text= 'Все' />
+			<Button classNames={getClass('active')} dataset={'active'} text= 'Активные' />
+			<Button classNames={getClass('done')}  dataset={'done'} text= 'Выполненные' />
 		</div>
 	)
 }
