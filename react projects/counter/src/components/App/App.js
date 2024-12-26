a44c2e966c2e3b52a87fb38fbf2700a3d4380e90
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-12-27 00:10:12 +0300

    react-projects: counter: render buttons on page

--- react projects/counter/src/components/App/App.js
+++ react projects/counter/src/components/App/App.js
@@ -3,37 +3,47 @@ import Button from './../Button/Button';
 import './App.css';
 
 const App = () => {
-  const buttons = [
-    {
-      id : 'btnMinus',
-      type : 'button',
-      classNames : 'btn btn-outline-primary',
-      text : 'Minus'
-    },
-    {
-      id : 'btnReset',
-      type : 'button',
-      classNames : 'btn btn-outline-primary',
-      text : 'Reset'
-    },
-    {
-      id : 'btnPlus',
-      type : 'button',
-      classNames : 'btn btn-outline-primary',
-      text : 'Plus'
-    }
-  ];
-  return (
-    <div className = 'app p-3'>
-      <h1>Counter</h1>
-      <Input/>
+const buttons = [
+	{
+	id : 'btnMinus',
+	type : 'button',
+	classNames : 'btn btn-outline-primary',
+	text : 'Minus'
+	},
+	{
+	id : 'btnReset',
+	type : 'button',
+	classNames : 'btn btn-outline-primary',
+	text : 'Reset'
+	},
+	{
+	id : 'btnPlus',
+	type : 'button',
+	classNames : 'btn btn-outline-primary',
+	text : 'Plus'
+	}
+];
 
-      <div className="btn-group" role="group" aria-label="Basic example">
-        <Button/>
-        <button type="button" className="btn btn-outline-primary" id="btnPlus">Plus</button>
-      </div>
-    </div>
-  )
+// Render al buttons on page`
+const renderButtons = buttons.map( (button) => {
+	return  <Button
+				id = { button.id}
+				type = {button.type}
+				classNames = {button.classNames}
+				text = {button.text}
+			/>
+});
+
+return (
+	<div className = 'app p-3'>
+	<h1>Counter</h1>
+	<Input/>
+
+	<div className="btn-group" role="group" aria-label="Basic example">
+		{renderButtons}
+	</div>
+	</div>
+)
 }
 
 export default App;
---
+++ react projects/counter/src/components/Button/Button.js
@@ -0,0 +1,7 @@
+const Button = (props) => {
+  return (
+    <button type={props.type} className={props.classNames} id={props.id}>{props.text}</button>
+  )
+}
+
+export default Button;
