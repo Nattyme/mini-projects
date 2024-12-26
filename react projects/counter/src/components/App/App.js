f56075f789792d1f8c101a9906c44444b97a7a98
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-12-27 00:00:55 +0300

    react-projects: counter: buttons objects array

--- react projects/counter/src/components/App/App.js
+++ react projects/counter/src/components/App/App.js
@@ -3,6 +3,26 @@ import Button from './../Button/Button';
 import './App.css';
 
 const App = () => {
+  const buttons = [
+    {
+      id : 'btnMinus',
+      type : 'button',
+      classNames : 'btn btn-outline-primary',
+      text : 'Minus'
+    },
+    {
+      id : 'btnReset',
+      type : 'button',
+      classNames : 'btn btn-outline-primary',
+      text : 'Reset'
+    },
+    {
+      id : 'btnPlus',
+      type : 'button',
+      classNames : 'btn btn-outline-primary',
+      text : 'Plus'
+    }
+  ];
   return (
     <div className = 'app p-3'>
       <h1>Counter</h1>
@@ -10,8 +30,6 @@ const App = () => {
 
       <div className="btn-group" role="group" aria-label="Basic example">
         <Button/>
-        <button type="button" className="btn btn-outline-primary" id="btnMinus">Minus</button>
-        <button type="button" className="btn btn-outline-primary" id="btnReset">Reset</button>
         <button type="button" className="btn btn-outline-primary" id="btnPlus">Plus</button>
       </div>
     </div>
