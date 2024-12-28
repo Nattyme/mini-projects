2f272d64bc87988c205f6dacb4bbf36e0077c363
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-12-29 01:55:49 +0300

    react-projects: react-blog: json server base

--- react projects/react-blog/src/pages/Home/Home.js
+++ react projects/react-blog/src/pages/Home/Home.js
@@ -1,20 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import data from './../../data/data.json';
 import BlogList from './../BlogList/BlogList';
 import './Home.css';
 
 const Home = () => {
   const [posts, setPosts] = useState(data.blog);
-
+  const [name, setName] = useState('Jim');
   const handleDelete = (id) => {
     const newPosts = posts.filter( (post) => post.id !== id);
     setPosts(newPosts);
   }
 
+  useEffect(()=>{
+    console.log('use effect run');
+    
+  }, [name]);
+
 
   return ( 
     <div className = "home">
       <BlogList posts={posts} handleDelete = {handleDelete}/>
+      <button onClick={()=>{setName('Tom')}}>{name}</button>
     </div>
    
   );
