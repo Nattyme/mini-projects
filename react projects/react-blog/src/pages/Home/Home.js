eca51f343fd74750d4afc74127c144da8e570322
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-12-29 02:54:12 +0300

    react-projects: react-blog: loading and error messgae display and hide

--- react projects/react-blog/src/pages/Home/Home.js
+++ react projects/react-blog/src/pages/Home/Home.js
@@ -4,20 +4,35 @@ import './Home.css';
 
 const Home = () => {
   const [posts, setPosts] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(()=>{
     console.log('use effect run');
     fetch('http://localhost:8000/posts').then((res)=>{
+      console.log(res);
+      if(res.ok !== true) {
+        throw Error('Could not fetch data from this resource')
+      }
+      
       return res.json();
     }).then((data) =>{
       console.log(data);
       setPosts(data);
+      setLoading(false);
+      setError(null);
+    }).catch((err)=>{
+      console.log(err.message);
+      setError(err.message);
+      setLoading(false);
     });
   }, []);
 
 
   return ( 
     <div className = "home">
+      {error && <div>{error}</div>}
+      {isLoading && <h3>Loading...</h3>}
       {posts && <BlogList posts={posts}/>}
     </div>
    
