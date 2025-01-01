a772fd28aed5e5d0af899fdbc0c4a46f57d99c17
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2025-01-02 01:57:45 +0300

    react-projects: react-blog: custom hoock 'useFetch'

--- react projects/react-blog/src/pages/Home/Home.js
+++ react projects/react-blog/src/pages/Home/Home.js
@@ -1,39 +1,16 @@
 import { useEffect, useState } from 'react';
 import BlogList from './../BlogList/BlogList';
+import useFetch from './../../userFetch'
 import './Home.css';
 
 const Home = () => {
-  const [posts, setPosts] = useState(null);
-  const [isLoading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(()=>{
-    console.log('use effect run');
-    fetch('http://localhost:8000/posts').then((res)=>{
-      console.log(res);
-      if(res.ok !== true) {
-        throw Error('Could not fetch data from this resource')
-      }
-      
-      return res.json();
-    }).then((data) =>{
-      console.log(data);
-      setPosts(data);
-      setLoading(false);
-      setError(null);
-    }).catch((err)=>{
-      console.log(err.message);
-      setError(err.message);
-      setLoading(false);
-    });
-  }, []);
-
+  const {data, isLoading, error} = useFetch('http://localhost:8000/posts');
 
   return ( 
     <div className = "home">
       {error && <div>{error}</div>}
       {isLoading && <h3>Loading...</h3>}
-      {posts && <BlogList posts={posts}/>}
+      {data && <BlogList posts={data}/>}
     </div>
    
   );
