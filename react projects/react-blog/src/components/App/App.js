872bf823a3e372a2430b6978a8cda7affd210411
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2025-01-02 02:10:11 +0300

    react-projects: react-blog: react-dom-router base data

--- react projects/react-blog/src/components/App/App.js
+++ react projects/react-blog/src/components/App/App.js
@@ -1,15 +1,21 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from '../../pages/Home/Home';
 import Navbar from './../Navbar/Navbar';
 import './App.css';
 
 function App() {
 	return (
-		<div className = "app">
-      <Navbar/>
-			<main className="main">
-          <Home/>
-			</main>
-		</div>
+    <Router>
+      <div className = "app">
+        <Navbar/>
+        <main className="main">
+           <Routes>
+              <Route path="/" elem={<Home/>}/>
+           </Routes>
+        </main>
+		  </div>
+    </Router>
+		
 	)
 }
 
