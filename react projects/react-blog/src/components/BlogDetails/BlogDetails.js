4e44f466cf6743b8218b597f9e112fa1e0f1bda3
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2025-01-07 17:28:49 +0300

    react-projects: react-blog: delete post from single post page

--- react projects/react-blog/src/components/BlogDetails/BlogDetails.js
+++ react projects/react-blog/src/components/BlogDetails/BlogDetails.js
@@ -1,9 +1,19 @@
 import {useParams} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import useFetch from '../../useFetch';
 
 const BlogDetails = () => {
   const {id} = useParams();
   const {data: blog, isLoading, error} = useFetch('http://localhost:8000/posts/' + id);
+  const navigate = useNavigate();
+
+  const deletePost = () => {
+    fetch('http://localhost:8000/posts/' + blog.id, {
+      method: 'DELETE'
+    }).then( () => {
+      navigate('/');
+    })
+  }
 
   return (
     <div className="blog-details">
@@ -15,6 +25,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p className="auther">Written by: {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={deletePost} className="btn-delete">Delete</button>
         </article>
       )}
     </div>
