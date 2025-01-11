1f3bbd3b9f814b7d529ac1ae3e078efc3896c4cf
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2025-01-12 01:56:09 +0300

    react-projects: cart: cart products state and base props data

--- react projects/cart/src/components/Cart/index.jsx
+++ react projects/cart/src/components/Cart/index.jsx
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import CartHeader from '../CartHeader';
 import Product from '../Product';
 import CartFooter from '../CartFooter';
+import data from './../../data/data.json';
 
 const Cart = () => {
+  const [cart, setCart] = useState(data);
+  const products = cart.map((product) => {
+    return  <Product product = {product} />
+  });
+  
 	return ( 
 		<section className="cart">
       
 			<CartHeader />
-      <Product />
+        {products}
       <CartFooter />
 
 		</section>
--- react projects/cart/src/components/Product/index.jsx
+++ react projects/cart/src/components/Product/index.jsx
@@ -2,18 +2,18 @@ import ButtonDelete from '../ButtonDelete';
 import Counter from '../Counter';
 import './style.scss';
 
-const Product = () => {
+const Product = ({product}) => {
   return ( 
 	<section className="product">
 		<div className="product__img">
-			<img src="./img/products/macbook.jpg" alt="Apple MacBook Air 13"/>
+			<img src="./img/products/macbook.jpg" alt={product.title}/>
 		</div>
-		<div className="product__title">Apple MacBook Air 13</div>
+		<div className="product__title">{product.title}</div>
 		<div className="product__count">
       <Counter />
 		</div>
 
-		<div className="product__price">110 000 руб.</div>
+		<div className="product__price">{product.price}</div>
 
 		<div className="product__controls">
       <ButtonDelete />
