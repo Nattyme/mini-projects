f7b88f095913b10d5e85abb2b6159008b8645036
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2025-02-02 00:43:05 +0300

    react-projects: counter-redux: state base and reducre function

---
+++ react projects/counter-redux/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import App from './App';
+
+// Дефолтное состояние
+const defaultState = {
+  counter: 0,
+}
+
+//Функция reducer
+// state - состояние
+// action - {type: "", payload: "?"}
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'INCREASE' :
+      return {...state, counter: state.counter + action.payload}
+    case 'DECREASE' :
+      return {...state, counter: state.counter - action.payload}
+    default: 
+      return state;
+  }
+}
+
+// Создаем store
+const store = createStore(reducer)
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider>
+    <App />
+  </Provider>
+);
